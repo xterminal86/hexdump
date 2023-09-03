6f644c9ed31df3f08f662918795ab6fd1ce37b34
xterminal86 <xterminal86@gmail.com>
2023-09-03 14:36:21 +0300

    small fix

--- hexdump.py
+++ hexdump.py
@@ -16,67 +16,66 @@ def main():
 
   limit32 = 2**32;
 
+  data = None;
+  
   with open(fname, "rb") as f:
     data = f.read();
 
-    ln = len(data);
+  ln = len(data);
 
-    counter = 0;
-    offset = 0;
+  counter = 0;
+  offset = 0;
 
-    padding = 8 if ln <= limit32 else 16;
-    lineWidth = padding + 2 + 23 + 2 + 23 + 2 + 18;
+  padding = 8 if ln <= limit32 else 16;
+  lineWidth = padding + 2 + 23 + 2 + 23 + 2 + 18;
 
-    print(f"{offset:0{padding}x}  ", end="");
+  print(f"{offset:0{padding}x}  ", end="");
 
-    chars = [ -1 ] * 16;
+  chars = [ -1 ] * 16;
 
-    groupSpaces = 0;
-    
-    for i in range(ln):
-      hexNum = f"{data[i]:02x}".upper();
+  groupSpaces = 0;
+  
+  for i in range(ln):
+    hexNum = f"{data[i]:02x}".upper();
 
-      if (data[i] >= 32 and data[i] < 127):
-        chars[counter] = data[i];
-      else:
-        chars[counter] = -1;
+    if (data[i] >= 32 and data[i] < 127):
+      chars[counter] = data[i];
+    else:
+      chars[counter] = -1;
 
-      print(f"{ hexNum } ", end="");
+    print(f"{ hexNum } ", end="");
 
-      counter += 1;
-      offset += 1;
+    counter += 1;
+    offset += 1;
 
-      if (counter == 8):
-        groupSpaces = 1;
-        print(" ", end="");
+    if (counter == 8):
+      groupSpaces = 1;
+      print(" ", end="");
 
-      if (counter == 16):
-        print(" ", end="");
-        print("|", end="");
-        for item in chars:
-          ch = "." if item == -1 else chr(item);
-          print(f"{ ch }", end="");
-        print("|", end="");
-        print("");
-        print(f"{offset:0{padding}x}  ", end="");
-        counter = 0;
-        groupSpaces = 0;
-
-    if (counter > 0):
-      additionalSpaces = lineWidth - 18 - (counter * 2 + (counter - 1)) - padding - (3 + groupSpaces);
-      spaces = " " * additionalSpaces;
-      print(f"{ spaces }", end="");
+    if (counter == 16):
+      print(" ", end="");
       print("|", end="");
-      for i in range(counter):
-        ch = "." if chars[i] == -1 else chr(chars[i]);
+      for item in chars:
+        ch = "." if item == -1 else chr(item);
         print(f"{ ch }", end="");
-
       print("|", end="");
+      print("");
+      print(f"{offset:0{padding}x}  ", end="");
+      counter = 0;
+      groupSpaces = 0;
+
+  if (counter > 0):
+    additionalSpaces = lineWidth - 18 - (counter * 2 + (counter - 1)) - padding - (3 + groupSpaces);
+    spaces = " " * additionalSpaces;
+    print(f"{ spaces }", end="");
+    print("|", end="");
+    for i in range(counter):
+      ch = "." if chars[i] == -1 else chr(chars[i]);
+      print(f"{ ch }", end="");
+
+    print("|", end="");
 
 ###############################################################################
 
 if __name__ == "__main__":
-  try:
-    main();
-  except:
-    pass;
+  main();
